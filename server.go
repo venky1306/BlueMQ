a5b4799222afa12adad22e2b58253fdd4f08e010
venky1306 <47674519+venky1306@users.noreply.github.com>
2023-12-29 00:35:25 +0530

    server config and methods

---
+++ server.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"golang.org/x/exp/slog"
+)
+
+type Message struct {
+	Topic string
+	Value []byte
+}
+
+type Config struct {
+	ListenAddr        string
+	StoreProducerFunc StoreProducerFunc
+}
+
+type Server struct {
+	*Config
+
+	topics map[string]Storer
+
+	consumers []Consumer
+	producers []Producer
+	producech <-chan Message
+	consumech <-chan Message
+	quitch    chan struct{}
+}
+
+func NewServer(c *Config) (*Server, error) {
+	producerch := make(chan Message)
+	consumech := make(chan Message)
+	return &Server{
+		Config:    c,
+		topics:    make(map[string]Storer),
+		producech: producerch,
+		consumech: consumech,
+		quitch:    make(chan struct{}),
+		consumers: []Consumer{
+			NewConsumer(c.ListenAddr, consumech),
+		},
+		producers: []Producer{
+			NewProducer(c.ListenAddr, producerch),
+		},
+	}, nil
+}
+
+func (s *Server) Start() error {
+	for _, p := range s.producers {
+		go p.Start()
+	}
+	for _, c := range s.consumers {
+		go c.Start()
+	}
+	for {
+		select {
+		case <-s.quitch:
+			return nil
+		case m := <-s.producech:
+			index, err := s.publish(m)
+			if err != nil {
+				slog.Error("Error publishing message: %v", err)
+			} else {
+				slog.Info("Published message to topic %s with index %d", m.Topic, index)
+			}
+			// case index := <-s.consumech:
+
+		}
+	}
+}
+
+func (s *Server) publish(msg Message) (int, error) {
+	storer, ok := s.topics[msg.Topic]
+	if !ok {
+		storer = s.StoreProducerFunc()
+		s.topics[msg.Topic] = storer
+	}
+	return storer.Put(msg.Value)
+}
+
+func (s *Server) consume(index int) (Message, error) {
+}
