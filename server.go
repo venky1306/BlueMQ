60857fb20421cfbe19f8da8c0fb7aa5cfcc83ee5
venky1306 <47674519+venky1306@users.noreply.github.com>
2023-12-30 03:25:52 +0530

    resolved bugs

--- server.go
+++ server.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
+
 	"golang.org/x/exp/slog"
 )
 
 type Message struct {
 	Topic string
 	Value []byte
+}
+
+type Request struct {
+	Topic string
+	Index int
 }
 
 type Config struct {
@@ -19,25 +26,25 @@ type Server struct {
 
 	topics map[string]Storer
 
-	consumers []Consumer
-	producers []Producer
-	producech <-chan Message
-	consumech <-chan Message
-	quitch    chan struct{}
+	// consumers []Consumer
+	producers  []Producer
+	producech  chan Message
+	consumech  chan Request
+	quitsignal chan struct{}
 }
 
 func NewServer(c *Config) (*Server, error) {
 	producerch := make(chan Message)
-	consumech := make(chan Message)
+	consumech := make(chan Request)
 	return &Server{
-		Config:    c,
-		topics:    make(map[string]Storer),
-		producech: producerch,
-		consumech: consumech,
-		quitch:    make(chan struct{}),
-		consumers: []Consumer{
-			NewConsumer(c.ListenAddr, consumech),
-		},
+		Config:     c,
+		topics:     make(map[string]Storer),
+		producech:  producerch,
+		consumech:  consumech,
+		quitsignal: make(chan struct{}),
+		// consumers: []Consumer{
+		// 	NewConsumer(c.ListenAddr, consumech),
+		// },
 		producers: []Producer{
 			NewProducer(c.ListenAddr, producerch),
 		},
@@ -46,24 +53,24 @@ func NewServer(c *Config) (*Server, error) {
 
 func (s *Server) Start() error {
 	for _, p := range s.producers {
+		// fmt.Println("starting producer")
 		go p.Start()
 	}
-	for _, c := range s.consumers {
-		go c.Start()
-	}
+	// // for _, c := range s.consumers {
+	// // 	go c.Start()
+	// // }
 	for {
 		select {
-		case <-s.quitch:
+		case <-s.quitsignal:
 			return nil
 		case m := <-s.producech:
 			index, err := s.publish(m)
 			if err != nil {
 				slog.Error("Error publishing message: %v", err)
 			} else {
-				slog.Info("Published message to topic %s with index %d", m.Topic, index)
+				slog.Info(fmt.Sprintf("Published message to topic %s with index %d", m.Topic, index))
 			}
 			// case index := <-s.consumech:
-
 		}
 	}
 }
@@ -78,4 +85,5 @@ func (s *Server) publish(msg Message) (int, error) {
 }
 
 func (s *Server) consume(index int) (Message, error) {
+	return Message{}, nil
 }
